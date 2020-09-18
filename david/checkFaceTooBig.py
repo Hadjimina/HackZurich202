895a0bf90bea9452932cc4bc7c7fcdab98bcb3b3
David Niederberger <davidn@ini.uzh.ch>
2020-09-19 00:15:24 +0200

    face-detection on fiiiire

--- david/checkFaceTooBig.py
+++ david/checkFaceTooBig.py
@@ -16,24 +16,23 @@ def find_biggest_face(img):
 
     cv2.imshow("Hello", img)
     cv2.waitKey(0)
-    return (len(faces), biggest_face)
+    return biggest_face
 
 
 
 def checkFaceTooBigMain():
-    # Returns a tuple (Number of faces -> Integer, Is face too big -> Boolean)
     img_path = sys.argv[1]
     try:
         img = cv2.imread(img_path)
     except:
         print("Face-too-big-checker could not read image file..")
         return
-    result = find_biggest_face(img)
-    face = result[1]
+    face = find_biggest_face(img)
     img_size = img.shape
-    face_too_big = False
+    print(img_size)
     if face[2] / img_size[0] > 0.3 or face[3] / img_size[1] > 0.3:
-        face_too_big = True
-    return (result[0, face_too_big])
+        return("Face is too big..")
+    else:
+        return 0
     
 
