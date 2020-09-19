be6b4d9c6fa952a53ee40220afd8ef9f2adf0086
David Niederberger <davidn@ini.uzh.ch>
2020-09-19 02:04:13 +0200

    number of people

--- david/checkFaceTooBig.py
+++ david/checkFaceTooBig.py
@@ -1,8 +1,10 @@
 import cv2
 import sys
+from keras.models import load_model
+import numpy as np
 
-def find_biggest_face(img):
-    face_cascade = cv2.CascadeClassifier("haarcascade.xml")
+def find_biggest_face(img, casc_path):
+    face_cascade = cv2.CascadeClassifier(casc_path)
     faces = face_cascade.detectMultiScale(img, 1.1, 4)
     size = 0
     biggest_face = None
@@ -12,23 +14,46 @@ def find_biggest_face(img):
             size = face_size
             biggest_face = face
     
-    return biggest_face
+    return (len(faces), biggest_face)
+
+def check_emotion(img):
+    emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
+    img = cv2.resize(img, (48,48))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("derp", img)
+    cv2.waitKey(0)
+    img = np.reshape(img, [1, img.shape[0], img.shape[1], 1])
+
+
+    model = load_model("model_v6_23.hdf5")
+    print(model.predict(img))
+    predicted_class = np.argmax(model.predict(img))
+    label_map = dict((v,k) for k,v in emotion_dict.items()) 
+    predicted_label = label_map[predicted_class]
+    print(predicted_label)
 
 
 
-def checkFaceTooBigMain():
-    img_path = sys.argv[1]
+def checkFaceTooBigMain(img_path, casc_path):
+    # Returns a tuple (Number of faces -> Integer, Is face too big -> Boolean)
     try:
         img = cv2.imread(img_path)
     except:
         print("Face-too-big-checker could not read image file..")
         return
-    face = find_biggest_face(img)
+    result = find_biggest_face(img, casc_path)
+    face = result[1]
     img_size = img.shape
-    print(img_size)
+    face_too_big = False
+    sub_image = img[face[1]:face[1]+face[3], face[0]:face[0]+face[2]]
+    check_emotion(sub_image)
     if face[2] / img_size[0] > 0.3 or face[3] / img_size[1] > 0.3:
-        return("Face is too big..")
-    else:
-        return 0
-    
+        face_too_big = True
+    return (result[0], face_too_big)
 
+
+def main():
+    checkFaceTooBigMain(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    main()
